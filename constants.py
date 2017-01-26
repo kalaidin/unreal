06bf067fd301f3edd2bb186b55d1c8bc318fa2de
Artyom Korkhov <artyom.korkhov@gmail.com>
2017-01-26 23:37:17 +0300

    changed: params actualized

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@
 
 import os
 
-LOCAL_T_MAX = 20 # repeat step size
+LOCAL_T_MAX = 2 # repeat step size
 RMSP_ALPHA = 0.99 # decay parameter for RMSProp
 RMSP_EPSILON = 0.1 # epsilon parameter for RMSProp
 CHECKPOINT_DIR = '/tmp/unreal_checkpoints'
@@ -11,13 +11,13 @@ INITIAL_ALPHA_LOW = 1e-4    # log_uniform low limit for learning rate
 INITIAL_ALPHA_HIGH = 5e-3   # log_uniform high limit for learning rate
 PARALLEL_SIZE = 8 # parallel thread size
 
-ENV_TYPE = 'lab' # 'lab' or 'gym' or 'maze'
+# ENV_TYPE = 'lab' # 'lab' or 'gym' or 'maze'
 #ENV_NAME = 'seekavoid_arena_01'
-ENV_NAME = 'stairway_to_melon'
+# ENV_NAME = 'stairway_to_melon'
 #ENV_NAME = 'nav_maze_static_01'
 
-#ENV_TYPE = 'gym'
-#ENV_NAME = 'Breakout-v0'
+ENV_TYPE = 'gym'
+ENV_NAME = 'Skiing-v0'
 
 PREPARE_SUBMIT = False  # should submit or not
 SUBMIT_DIR = "/tmp/gym_submits/"
@@ -27,8 +27,8 @@ SUBMIT_OUTPUT = os.path.join(SUBMIT_DIR, "{}_{}".format(ENV_NAME, SUBMIT_VERSION
 INITIAL_ALPHA_LOG_RATE = 0.5 # log_uniform interpolate rate for learning rate
 GAMMA = 0.99 # discount factor for rewards
 ENTROPY_BETA = 0.001 # entropy regurarlization constant
-PIXEL_CHANGE_LAMBDA = 0.05 # 0.01 ~ 0.1 for Lab, 0.0001 ~ 0.01 for Gym
-EXPERIENCE_HISTORY_SIZE = 2000 # Experience replay buffer size
+PIXEL_CHANGE_LAMBDA = 0.1 # 0.01 ~ 0.1 for Lab, 0.0001 ~ 0.01 for Gym
+EXPERIENCE_HISTORY_SIZE = 10000 # Experience replay buffer size
 
 USE_PIXEL_CHANGE      = True
 USE_VALUE_REPLAY      = True
